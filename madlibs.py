7cedfef1f0c3a2788149e327baad7845c26e3cdd
Nzx03 <nzx786@gmail.com>
2024-10-18 00:13:40 +0530

    Add files via upload

--- madlibs.py
+++ madlibs.py
@@ -1,3 +1,25 @@
-with open(r"madlibs-generator\story.txt", "r") as file:
-    content = file.read()
-    print(content)
+with open(r'C:\Users\Nazneen\git\madlibs-generator\story.txt', "r") as file:
+    # content = file.read()
+    # print(content)
+   collection=[]    
+
+
+   for line in file.readlines():
+          words=line.split()
+          for word in words:
+             if word.startswith('<'):
+                collection.append(word)
+   print(collection)
+         
+
+   
+    
+    
+
+
+
+
+        
+
+
+    
